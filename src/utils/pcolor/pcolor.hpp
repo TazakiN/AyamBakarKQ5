75e251d89b0a25901b40a816d73a13e5f3f883a5
NopalAul <mnaufal0605@gmail.com>
2024-04-01 16:07:34 +0700

    feat: add pcolor

---
+++ src/utils/pcolor/pcolor.hpp
@@ -0,0 +1,23 @@
+#ifndef PCOLOR_HPP
+#define PCOLOR_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string p_red();
+
+string p_green();
+
+string p_blue();
+
+string p_yellow();
+
+string p_magenta();
+
+string p_rgb(int r, int g, int b);
+
+string reset();
+
+#endif 
