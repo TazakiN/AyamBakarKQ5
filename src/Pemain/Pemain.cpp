4bc5db76b079b5bcc39167caa3084df35a1f3de0
denoseu <denisefeliciatiowanni@gmail.com>
2024-04-03 12:52:30 +0700

    feat: pemain

--- src/Pemain/Pemain.cpp
+++ src/Pemain/Pemain.cpp
@@ -61,11 +61,11 @@ void Pemain::masukanItem(string nama_item, int idx_row, int idx_col) {
 }
 
 
-void Pemain::beli(string nama_item, int kuantitas, int idx_row, int idx_col) 
+void Pemain::beli(Item nama_item, int kuantitas, int idx_row, int idx_col) 
 {
     // cek apakah inventory cukup utk membeli
     if (inventory->isEmpty(kuantitas)) {
-        cout << "Inventory Anda tidak cukup untuk membeli " << kuantitas << " " << nama_item << "." << endl;
+        cout << "Inventory Anda tidak cukup untuk membeli " << kuantitas << " " << nama_item.getName() << "." << endl;
         return;
     }
 
@@ -76,21 +76,21 @@ void Pemain::beli(string nama_item, int kuantitas, int idx_row, int idx_col)
 
         // cek apakah gulden cukup
         if (gulden < total_harga) {
-            cout << "Uang Anda tidak cukup untuk membeli " << kuantitas << " " << nama_item << "." << endl;
+            cout << "Uang Anda tidak cukup untuk membeli " << kuantitas << " " << nama_item.getName() << "." << endl;
             return;
         }
 
         // membeli barang
         for (int i = 0; i < kuantitas; i++) {
-            inventory->setItem(idx_row, idx_col + i, new Item(nama_item, "kode")); // "kode" masih temp, bingung gimana dapet kode?
+            inventory->setItem(idx_row, idx_col + i, new Item(nama_item));
         }
 
         // kurangi uang
         kurangiGulden(total_harga);
-        cout << "Selamat Anda berhasil membeli " << kuantitas << " " << nama_item << ". Uang Anda tersisa " << gulden << " gulden." << endl;
+        cout << "Selamat Anda berhasil membeli " << kuantitas << " " << nama_item.getName() << ". Uang Anda tersisa " << gulden << " gulden." << endl;
 }
 
-void Pemain::jual(string nama_item, int kuantitas, int idx_row, int idx_col)
+void Pemain::jual(Item nama_item, int kuantitas, int idx_row, int idx_col)
 {
     // itung total harga jual
     // int harga_per_barang = toko.getHarga(nama_item);
@@ -109,45 +109,49 @@ void Pemain::jual(string nama_item, int kuantitas, int idx_row, int idx_col)
 }
 
 void Pemain::makan() {
-    // menampilkan isi penyimpanan
-    cout << "Pilih makanan dari penyimpanan" << endl;
-    inventory->printGridHeader();
-    inventory->printGrid();
+    while (true) {
+        // menampilkan isi penyimpanan
+        cout << "Pilih makanan dari penyimpanan" << endl;
+        inventory->printGridHeader();
+        inventory->printGrid();
 
-    // memilih makanan
-    string slot;
-    cout << "\nSlot: ";
-    cin >> slot;
+        // memilih makanan
+        string slot;
+        cout << "\nSlot: ";
+        cin >> slot;
 
-    // cek apakah slot yang dipilih valid
-    int row = slot[0] - 'A';
-    int col = stoi(slot.substr(1)) - 1;
-    if (row < 0 || row >= inventory->getRow() || col < 0 || col >= inventory->getCol()) {
-        cout << "Kamu mengambil harapan kosong dari penyimpanan." << endl;
-        return;
-    }
+        // cek apakah slot yang dipilih valid
+        int row = slot[0] - 'A';
+        int col = stoi(slot.substr(1)) - 1;
+        if (row < 0 || row >= inventory->getRow() || col < 0 || col >= inventory->getCol()) {
+            cout << "Kamu mengambil harapan kosong dari penyimpanan." << endl;
+            cout << "Silahkan masukan slot yang berisi makanan." << endl;
+            continue;
+        }
 
-    // cek apakah ada makanan di slot yang dipilih
-    Item* item = inventory->getItem(row, col);
-    Produk* produk = dynamic_cast<Produk*>(item);
+        // cek apakah ada makanan di slot yang dipilih
+        Item* item = inventory->getItem(row, col);
+        Produk* produk = dynamic_cast<Produk*>(item);
 
-    if (produk) {
-        if (produk->isMakanan() == false) {
-            cout << "Apa yang kamu lakukan??!! Kamu mencoba untuk memakan itu?!!" << endl;
-            return;
-        } else {
-            // makan makanan
-            string nama_makanan = item->getName();
-            // int tambahan_berat_badan = gimana ambil tambahan berat badan dari makanan?
-            int tambahan_berat_badan = 5; // sementara
-            tambahBeratBadan(tambahan_berat_badan);
+        if (produk) {
+            if (produk->isMakanan() == false) {
+                cout << "Apa yang kamu lakukan??!! Kamu mencoba untuk memakan itu?!!" << endl;
+                cout << "Silahkan masukan slot yang berisi makanan." << endl;
+                continue;
+            } else {
+                // makan makanan
+                string nama_makanan = item->getName();
+                // int tambahan_berat_badan = gimana ambil tambahan berat badan dari makanan?
+                int tambahan_berat_badan = 5; // sementara
+                tambahBeratBadan(tambahan_berat_badan);
 
-            // Menghapus makanan dari penyimpanan
-            inventory->removeItem(row, col);
+                // Menghapus makanan dari penyimpanan
+                inventory->removeItem(row, col);
 
-            cout << "\nDengan lahapnya, kamu memakan hidangan " << nama_makanan << endl;
-            cout << "Alhasil, berat badan kamu naik menjadi " << berat_badan << endl;
+                cout << "\nDengan lahapnya, kamu memakan hidangan " << nama_makanan << endl;
+                cout << "Alhasil, berat badan kamu naik menjadi " << berat_badan << endl;
+                break;
+            }
         }
     }
-
 }
