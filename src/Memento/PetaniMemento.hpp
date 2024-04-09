2dd7255b0021b9d935c2b9055b3aae05e8d1473a
Steven <98434141+Zechtro@users.noreply.github.com>
2024-04-09 17:59:22 +0700

    feat: memento petani header

--- src/Memento/PetaniMemento.hpp
+++ src/Memento/PetaniMemento.hpp
@@ -1,14 +1,35 @@
 #ifndef PETANIMEMENTO_HPP
 #define PETANIMEMENTO_HPP
 
-// TO DO : include class Ladang
+#include "../Grid/Ladang.hpp"
 
 using namespace std;
 
 class PetaniMemento{
-    // private:
-        // TO DO : Ladang ladang;
+    private:
+        Ladang* ladang;
 
+    public:
+        /**
+         * @brief Konstruktor
+         * 
+         * @param pemainLadang  Keadaan ladang petani sebelum melakukan suatu action
+         */
+        PetaniMemento(Ladang& pemainLadang);
+
+
+        /**
+         * @brief Destruktor
+         * 
+         */
+        ~PetaniMemento();
+        
+        /**
+         * @brief Mengembalikan keadaan ladang pemain menjadi keadaan ladang pada memento
+         * 
+         * @param pemainLadang  Ladang pemain saat ini
+         */
+        void undoLadang(Ladang* pemainLadang);
 };
 
 #endif
