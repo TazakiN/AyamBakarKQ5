d4b68f31d9ebe49ba68bff27350fcc6c7b78a961
Steven <98434141+Zechtro@users.noreply.github.com>
2024-04-11 00:11:26 +0700

    feat: action history implementation

--- src/Memento/ActionHistory.cpp
+++ src/Memento/ActionHistory.cpp
@@ -1,3 +1,24 @@
 #include "ActionHistory.hpp"
+#include "Memento.hpp"
 
 using namespace std;
+
+ActionHistory::ActionHistory(){
+
+};
+
+ActionHistory::~ActionHistory(){
+    while(!this->stack_memento.empty()){
+        this->stack_memento.pop();
+    } 
+};
+
+void ActionHistory::pushMemento(Memento* pm){
+    this->stack_memento.push(pm);
+};
+
+Memento* ActionHistory::popMemento(){
+    Memento* temp = this->stack_memento.top();
+    this->stack_memento.pop();
+    return temp;
+};
