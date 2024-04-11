66185e41afabb98533274af9638b8f9573521742
Steven <98434141+Zechtro@users.noreply.github.com>
2024-04-12 00:38:53 +0700

    feat: memento petani implementation

--- src/Memento/PetaniMemento.cpp
+++ src/Memento/PetaniMemento.cpp
@@ -1,3 +1,22 @@
 #include "PetaniMemento.hpp"
 
 using namespace std;
+
+PetaniMemento::PetaniMemento(Ladang& pemainLadang){
+    this->ladang = new Ladang(pemainLadang);
+};
+
+PetaniMemento::~PetaniMemento(){
+    delete ladang;
+};
+
+void PetaniMemento::undoLadang(Ladang* pemainLadang){
+    int i;
+    int j;
+    for(i=0;i<this->ladang->getRow();i++){
+        for(j=0;j<this->ladang->getCol();j++){
+            pemainLadang->setItem(i,j,this->ladang->getItem(i,j));
+        }
+    }
+};
+
