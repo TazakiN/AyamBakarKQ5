ca51d6a639d2725707c2fb1b55c393c74ddd9634
Steven <98434141+Zechtro@users.noreply.github.com>
2024-04-12 00:59:27 +0700

    feat: memento walikota implementation

--- src/Memento/WalikotaMemento.cpp
+++ src/Memento/WalikotaMemento.cpp
@@ -1,3 +1,16 @@
 #include "WalikotaMemento.hpp"
 
 using namespace std;
+
+WalikotaMemento::WalikotaMemento(){
+
+};
+
+WalikotaMemento::~WalikotaMemento(){
+    // this->map_pemain_gulden.clear();
+};
+
+void WalikotaMemento::insertPemainGulden(string pemain, int gulden){
+    this->map_pemain_gulden.insert(pair<string,int>(pemain,gulden));
+};
+
