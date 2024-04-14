aa4b30e3adaf7e68dcb1479836ff414399f51516
Steven <98434141+Zechtro@users.noreply.github.com>
2024-04-14 07:45:28 +0700

    fix: add memento inventory copy

--- src/Memento/Memento.cpp
+++ src/Memento/Memento.cpp
@@ -1,8 +1,14 @@
 #include "Memento.hpp"
 
 Memento::Memento(Inventory& pemain_inventory, int pemain_bb, int pemain_gulden, Toko& toko){
-    // CCTOR INVENTORY
-    this->inventory = new Inventory(pemain_inventory);
+    this->inventory = new Inventory(pemain_inventory.getRow(),pemain_inventory.getCol());
+    int i;
+    int j;
+    for(i=0;i<this->inventory->getRow();i++){
+        for(j=0;j<this->inventory->getCol();j++){
+            this->inventory->setItem(i,j,pemain_inventory.getItem(i,j));
+        }
+    }
     this->berat_badan = pemain_bb;
     this->gulden = pemain_gulden;
     // CCTOR TOKO
