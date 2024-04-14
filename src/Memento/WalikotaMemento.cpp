70e3fd9cb724fae6c5371c6e719f7ca8eb54a337
TazakiN <tazkia.nizami@gmail.com>
2024-04-14 23:24:40 +0700

    fix: compile Memento error

--- src/Memento/WalikotaMemento.cpp
+++ src/Memento/WalikotaMemento.cpp
@@ -2,30 +2,34 @@
 
 using namespace std;
 
-WalikotaMemento::WalikotaMemento(Inventory& pemain_inventory, int pemain_bb, int pemain_gulden, Toko& toko) : Memento(pemain_inventory, pemain_bb, pemain_gulden, toko){
+WalikotaMemento::WalikotaMemento(Inventory &pemain_inventory, int pemain_bb, int pemain_gulden, Toko &toko) : Memento(pemain_inventory, pemain_bb, pemain_gulden, &toko){
 
-};
+                                                                                                              };
 
 WalikotaMemento::~WalikotaMemento(){
     // this->map_pemain_gulden.clear();
 };
 
-void WalikotaMemento::insertPemainGulden(string pemain, int gulden){
-    this->map_pemain_gulden.insert(pair<string,int>(pemain,gulden));
+void WalikotaMemento::insertPemainGulden(string pemain, int gulden)
+{
+    this->map_pemain_gulden.insert(pair<string, int>(pemain, gulden));
 };
 
-void WalikotaMemento::insertCreatedPemain(Pemain* p){
+void WalikotaMemento::insertCreatedPemain(Pemain *p)
+{
     this->created_pemain.push_back(p);
 }
 
-void WalikotaMemento::deleteCreatedPemain(){
+void WalikotaMemento::deleteCreatedPemain()
+{
     while (!this->created_pemain.empty())
     {
-        delete this->created_pemain.at(this->created_pemain.size()-1);
+        delete this->created_pemain.at(this->created_pemain.size() - 1);
         this->created_pemain.pop_back();
     }
 }
 
-void WalikotaMemento::undoGuldenPemain(Pemain* p){
-    p->tambahkanGulden(this->map_pemain_gulden[p->getName()]-p->getGulden());
+void WalikotaMemento::undoGuldenPemain(Pemain *p)
+{
+    p->tambahkanGulden(this->map_pemain_gulden[p->getName()] - p->getGulden());
 }
