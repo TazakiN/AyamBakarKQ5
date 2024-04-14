d719820b86d7a0a66dffc1ecfdb771dcf843eb00
TazakiN <tazkia.nizami@gmail.com>
2024-04-14 23:08:40 +0700

    fix: add trim to fix Inventory

--- src/Memento/Memento.cpp
+++ src/Memento/Memento.cpp
@@ -1,12 +1,15 @@
 #include "Memento.hpp"
 
-Memento::Memento(Inventory& pemain_inventory, int pemain_bb, int pemain_gulden, Toko* tokoAwal){
-    this->inventory = new Inventory(pemain_inventory.getRow(),pemain_inventory.getCol());
+Memento::Memento(Inventory &pemain_inventory, int pemain_bb, int pemain_gulden, Toko *tokoAwal)
+{
+    this->inventory = new Inventory(pemain_inventory.getRow(), pemain_inventory.getCol());
     int i;
     int j;
-    for(i=0;i<this->inventory->getRow();i++){
-        for(j=0;j<this->inventory->getCol();j++){
-            this->inventory->setItem(i,j,pemain_inventory.getItem(i,j));
+    for (i = 0; i < this->inventory->getRow(); i++)
+    {
+        for (j = 0; j < this->inventory->getCol(); j++)
+        {
+            this->inventory->setItem(i, j, pemain_inventory.getItem(i, j));
         }
     }
     this->berat_badan = pemain_bb;
@@ -15,40 +18,50 @@ Memento::Memento(Inventory& pemain_inventory, int pemain_bb, int pemain_gulden, 
     this->toko->copyToko(tokoAwal);
 };
 
-Memento::~Memento(){
+Memento::~Memento()
+{
     delete this->inventory;
     delete this->toko;
 };
 
-void Memento::insertCreatedItem(Item* item){
+void Memento::insertCreatedItem(Item *item)
+{
     created_items.push_back(item);
 }
 
-void Memento::undoInventory(Inventory* pemain_inventory){
+void Memento::undoInventory(Inventory *pemain_inventory)
+{
     int i;
     int j;
-    for(i=0;i<this->inventory->getRow();i++){
-        for(j=0;j<this->inventory->getCol();j++){
-            pemain_inventory->setItem(i,j,this->inventory->getItem(i,j));
+    for (i = 0; i < this->inventory->getRow(); i++)
+    {
+        for (j = 0; j < this->inventory->getCol(); j++)
+        {
+            pemain_inventory->setItem(i, j, this->inventory->getItem(i, j));
         }
     }
 }
 
-int Memento::getBeratBadanMemento(){
+int Memento::getBeratBadanMemento()
+{
     return this->berat_badan;
 }
 
-int Memento::getGuldenMemento(){
+int Memento::getGuldenMemento()
+{
     return this->gulden;
 }
 
-void undoToko(Toko* tokoGame, Memento* m){
+void undoToko(Toko *tokoGame, Memento *m)
+{
     tokoGame->copyToko(m->toko);
 }
 
-void Memento::deleteCreatedItems(){
-    while(!this->created_items.empty()){
-        delete this->created_items.at(this->created_items.size()-1);
+void Memento::deleteCreatedItems()
+{
+    while (!this->created_items.empty())
+    {
+        delete this->created_items.at(this->created_items.size() - 1);
         this->created_items.pop_back();
     }
 }
--- src/Memento/Memento.hpp
+++ src/Memento/Memento.hpp
@@ -8,81 +8,82 @@
 
 using namespace std;
 
-class Memento{
-    private:
-        Inventory* inventory;
-        int berat_badan;
-        int gulden;
-        Toko* toko;
-        vector<Item*> created_items;
-        // vector<Item*> deleted_items;
-        
-    public:
-        /**
-         * @brief Construct a new Memento object
-         * 
-         * @param pemain_inventory  keadaan inventory pemain sebelum suatu action, nanti akan digunakan dengan copy constructor
-         * @param pemain_bb         keadaan berat badan pemain sebelum suatu action
-         * @param pemain_gulden     keadaan gulden pemain sebelum suatu action
-         * @param toko              keadaan toko sebelum suatu action
-         */
-        Memento(Inventory& pemain_inventory, int pemain_bb, int pemain_gulden, Toko* toko);
+class Memento
+{
+private:
+    Inventory *inventory;
+    int berat_badan;
+    int gulden;
+    Toko *toko;
+    vector<Item *> created_items;
+    // vector<Item*> deleted_items;
 
-        /**
-         * @brief Destruktor
-         * 
-         */
-        ~Memento();
+public:
+    /**
+     * @brief Construct a new Memento object
+     *
+     * @param pemain_inventory  keadaan inventory pemain sebelum suatu action, nanti akan digunakan dengan copy constructor
+     * @param pemain_bb         keadaan berat badan pemain sebelum suatu action
+     * @param pemain_gulden     keadaan gulden pemain sebelum suatu action
+     * @param toko              keadaan toko sebelum suatu action
+     */
+    Memento(Inventory &pemain_inventory, int pemain_bb, int pemain_gulden, Toko *toko);
 
-        /**
-         * @brief Merekap item yang terbuat setelah suatu action
-         * 
-         * @param item  item yang terbuat setelah suatu action 
-         */
-        void insertCreatedItem(Item* item);
+    /**
+     * @brief Destruktor
+     *
+     */
+    ~Memento();
 
-        /**
-         * @brief Merekap item yang terhapus setelah suatu action, setiap item akan dilakukan copy constructor
-         * 
-         * @param item 
-         */
-        // void insertDeletedItem(Item* item);
+    /**
+     * @brief Merekap item yang terbuat setelah suatu action
+     *
+     * @param item  item yang terbuat setelah suatu action
+     */
+    void insertCreatedItem(Item *item);
 
-        /**
-         * @brief Mengembalikan keadaan inventory pemain saat ini menjadi keadaan inventory pada memento
-         * 
-         * @param pemain_inventory 
-         */
-        void undoInventory(Inventory* pemain_inventory);
+    /**
+     * @brief Merekap item yang terhapus setelah suatu action, setiap item akan dilakukan copy constructor
+     *
+     * @param item
+     */
+    // void insertDeletedItem(Item* item);
 
-        /**
-         * @brief Mendapatkan keadaan berat badan di memento
-         * 
-         * @return int 
-         */
-        int getBeratBadanMemento();
+    /**
+     * @brief Mengembalikan keadaan inventory pemain saat ini menjadi keadaan inventory pada memento
+     *
+     * @param pemain_inventory
+     */
+    void undoInventory(Inventory *pemain_inventory);
 
-        /**
-         * @brief Mendapatkan keadaan gulden di memento
-         * 
-         * @return int 
-         */
-        int getGuldenMemento();
+    /**
+     * @brief Mendapatkan keadaan berat badan di memento
+     *
+     * @return int
+     */
+    int getBeratBadanMemento();
 
-        /**
-         * @brief Mengembalikan keadaan toko dengan keadaan toko di memento
-         * 
-         * @param toko 
-         */
-        friend void undoToko(Toko* tokoGame, Memento* m);
+    /**
+     * @brief Mendapatkan keadaan gulden di memento
+     *
+     * @return int
+     */
+    int getGuldenMemento();
 
-        /**
-         * @brief Menghapus item yang telah terbuat
-         * 
-         */
-        void deleteCreatedItems();
+    /**
+     * @brief Mengembalikan keadaan toko dengan keadaan toko di memento
+     *
+     * @param toko
+     */
+    friend void undoToko(Toko *tokoGame, Memento *m);
 
-        // void createDeletedItems();
+    /**
+     * @brief Menghapus item yang telah terbuat
+     *
+     */
+    void deleteCreatedItems();
+
+    // void createDeletedItems();
 };
 
 #endif
