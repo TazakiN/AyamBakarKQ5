97b1ad8954973cb81b124bd3cfbc4843ca0ff08b
NopalAul <mnaufal0605@gmail.com>
2024-04-15 20:59:26 +0700

    Update anim.hpp

--- src/utils/anim.hpp
+++ src/utils/anim.hpp
@@ -9,37 +9,4 @@
 #include <vector>
 #include "pcolor.hpp"
 
-
-string title1;
-
-string code1;
-
-string title2;
-
-string code2;
-
-string homeImage;
-
-string homePage1;
-
-string homePage2;
-
-string welcome;
-
-string peternak;
-
-string peternak2;
-
-string petani;
-
-string petani2;
-
-string walikota;
-
-string walikota2;
-
-void printLoadingAnimation();
-
-void printAnimation(const string& frame1, const string& frame2, int numFrames, int delay);
-
 #endif 
